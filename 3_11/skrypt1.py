5131dd2001abb46d53f35dad4b9bcd447b8826db
kradziko <blena.dzidzia@gmail.com>
2016-11-03 18:10:45 +0100

    moduly - random

--- 3_11/skrypt1.py
+++ 3_11/skrypt1.py
@@ -1,21 +1,21 @@
 '''moduly - generator liczb pseudolosowch'''
 
-#import random
+import random
 
-from random import *
+#from random import *
 #from random import randint
 
 random.seed()
-print random.randinit(1,15) #losuje liczbe calkowita z zakresu 1, 15
-print random.randinit(1,15)
+print random.randint(1,15) #losuje liczbe calkowita z zakresu 1, 15
+print random.randint(1,15)
 
 l = range(1,10)
-print random.choice(l)
-random.shuffle(l)
+print random.choice(l) # wybiera losowy element z sekwencji
+random.shuffle(l) # wykonuje losowa permutacja z sekwencji
 print l
 
-print random.random()
+print random.random()  #liczba rzeczywista z zakresu od 0.0 do 1.0
 
-print random.uniform(10,30)
+print random.uniform(10,30) #losowa liczba rzeczywista z przedzialu [10,30]
 
-print random.normalvariate(5,48)
+print random.normalvariate(5,48) #zmienna losowa o rozkladzie normalnym o sredniej 5 i odchyleniu 48
