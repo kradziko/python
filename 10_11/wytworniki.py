51b514a108f3a25a2c805a340c8912f05315e3e3
kradziko <blena.dzidzia@gmail.com>
2016-12-01 14:48:13 +0100

    10.11.2016

--- 10_11/wytworniki.py
+++ 10_11/wytworniki.py
@@ -3,74 +3,73 @@
 wytworniki (list comprehensions)
 '''
 
-l=range(1,21,2)
+l = range(1, 21, 2)
 print l
 
-#postac prosta
+# postac prosta
 
-#podwojenie wartosci
-print[2*x for x in l]
+# podwojenie wartosci
+print[2 * x for x in l]
 
-#para x, kwadrat x
-print [(x,x*x) for x in range(1,5)]
+# para x, kwadrat x
+print [(x, x * x) for x in range(1, 5)]
 
 # tabela kodowa ASCII
-print[(x, ord(x))for x in "ABCDEF"]
+print[(x, ord(x)) for x in "ABCDEF"]
 
-#lista zawierajaca 10 pustych list
-print [ [] for x in range(10)]
+# lista zawierajaca 10 pustych list
+print [[] for x in range(10)]
 
-#posta prosta warunkowa
+# posta prosta warunkowa
 
-#liczby wieksze od 10
-print [x for x in l if x>10]
+# liczby wieksze od 10
+print [x for x in l if x > 10]
 
-#liczby podzielne przez 3 lub 5
-print [x for x in range(1,20) if not (x%3) or not (x%5)]
+# liczby podzielne przez 3 lub 5
+print [x for x in range(1, 20) if not (x % 3) or not (x % 5)]
 
-#tabela kodowa ASCII dla samoglosek
-print [(x, ord(x))for x in "ABCDEF" if x in "AEIOUY"]
+# tabela kodowa ASCII dla samoglosek
+print [(x, ord(x)) for x in "ABCDEF" if x in "AEIOUY"]
 
-#postac rozszerzona
+# postac rozszerzona
 
-#pary kazdy element z kazdym
-print [(x,y) for x in range(1,5)
-    for y in range(4,0,-1)]
+# pary kazdy element z kazdym
+print [(x, y) for x in range(1, 5)
+       for y in range(4, 0, -1)]
 
-#roznica miedzy wartoscia z pierwsze i drugiej listy
-print [x-y for z in range(1,5)
-       for y in range(4,0,-1)]
+# roznica miedzy wartoscia z pierwsze i drugiej listy
+print [x - y for z in range(1, 5)
+       for y in range(4, 0, -1)]
 
-#sklejenie napisu z wartosci pochodzacych z trzech list
-print ['x'+y+'z' for x in [1,2]
+# sklejenie napisu z wartosci pochodzacych z trzech list
+print ['x' + y + 'z' for x in [1, 2]
        for y in ['A', 'B']
-       for z in [0,3] ]
+       for z in [0, 3]]
 
-#postac rozszerzona z jednym warunkiem
+# postac rozszerzona z jednym warunkiem
 
-#para kazdy element z kazdym tylko jezeli pierwszy
-#element jest mniejszy od drugiego
-print [(x,y) for x in range(1,5)
-       for y in range(6,3,-1)
-       if x<y]
+# para kazdy element z kazdym tylko jezeli pierwszy
+# element jest mniejszy od drugiego
+print [(x, y) for x in range(1, 5)
+       for y in range(6, 3, -1)
+       if x < y]
 
-#para kazdy element z kazdym tylko jezeli
-#suma eelementow jest mniejsza od 7
-print [(x,y) for x in range(1,5)
-       for y in range(6,3,-1)
-       if x+y<7]
+# para kazdy element z kazdym tylko jezeli
+# suma elementow jest mniejsza od 7
+print [(x, y) for x in range(1, 5)
+       for y in range(6, 3, -1)
+       if x + y < 7]
 
-#para kazdy element z kazdym pod warunkiem ze
-#pierwszy element jest parzysty lub drugi jest nie parzysty
-print [(x,y) for x in range(1,5)
-       for y in range(6,2,-1)
-       if not (x%2) or y%2]
+# para kazdy element z kazdym pod warunkiem ze
+# pierwszy element jest parzysty lub drugi jest nie parzysty
+print [(x, y) for x in range(1, 5)
+       for y in range(6, 2, -1)
+       if not (x % 2) or y % 2]
 
-#postac rozszerzona z wieloma warunkami
+# postac rozszerzona z wieloma warunkami
 
-#para kazdy element z kazdym pod warunkiem ze pierwszy
-#element jest parzysty a drugi jest nie parzysty
+# para kazdy element z kazdym pod warunkiem ze pierwszy
+# element jest parzysty a drugi jest nie parzysty
 
-print [(x,y) for x in range(1,5) if not (x%2)
-       for y in range(6,2,-1) if y%2]
-
+print [(x, y) for x in range(1, 5) if not (x % 2)
+       for y in range(6, 2, -1) if y % 2]
--- 10_11/zadanie.py
+++ 10_11/zadanie.py
@@ -8,7 +8,7 @@ e) wyszuka duplikaty z kazdej listy i utworzy z nich nowa liste
 f) zastapi duplikaty znakiem 'X' z listy a)
 g) usunie duplikaty z listy a)
 h) obliczy wartosc srednia i podniesie kazda wartosc do potegi 3
-i) zastapi wielokrotnosci liczby 2 listera A, 3 litera E, 5 litera L
+i) zastapi wielokrotnosci liczby 2 litera A, 3 litera E, 5 litera L
 j) wyszuka liczby pierwsze i zastapi je listera Z
 k) wyszuka litery w liscie i utworzy z nich losowe napisy
 '''
@@ -17,6 +17,15 @@ import random
 #from random import *
 #from random import randint
 
+def czy_pierwsza(liczba):
+    if liczba<2:
+        return False
+    i = 2
+    while(i*i<=liczba):
+        if liczba%i == 0:
+            return False
+        i += 1
+    return True
 
 class listy:
     l =[]
@@ -41,25 +50,86 @@ class listy:
         return slownik
     def duplikaty_do_nowej(self,slow):
         lista_duplikatow = []
-
         for key, value in slow.iteritems():
-            for x in range(0,len(slow[key])):
-                a = slow[key][x]
-                #for y in range(1, len(slow[key])):
-                if a == slow[key][x]:
-                    lista_duplikatow.append(a)
-                a = slow[key][x]
+            for x in range(0, len(value)):
+                a = value[x]
+                for y in range(x + 1, len(value)):
+                    if a == value[y]:
+                        if value[y] not in lista_duplikatow:
+                            lista_duplikatow.append(value[y])
         return lista_duplikatow
+    def duplikaty_na_X(self):
+        for x in range(0, len(self.l)):
+            a = self.l[x]
+            for y in range(x + 1, len(self.l)):
+                if a == self.l[y]:
+                    self.l[y]="X"
+    def usun_duplikaty(self):
+        for x in range(0, len(self.l)):
+            a = self.l[x]
+            for y in range(x+1, len(self.l)):
+                if a == self.l[y]:
+                    print a
+                    #jak usunac elementy ?
+    def srednia_i_do_potegi_3(self):
+        suma = 0
+        for x in range(0, len(self.l)):
+            suma = suma + self.l[x]
+            self.l[x] = pow(self.l[x],3)
+        return suma/float(len(self.l))
+    def zastap_wielokrotnosci_235_AEL(self,x =2,y=3,z=5,*inne):
+        for t in range(0, len(self.l)):
+            if not (self.l[t] % x):
+                self.l[t] = "A"
+            if isinstance(self.l[t],int) and not (self.l[t] % y):
+                self.l[t] = "E"
+            if isinstance(self.l[t],int) and not (self.l[t] % z):
+                self.l[t] = "L"
+    def zastap_pierwsze_Z(self):
+        for t in range(0, len(self.l)):
+            if isinstance(self.l[t],int) and czy_pierwsza(self.l[t]):
+                self.l[t]="Z"
+    def losowe_napisy(self):
+        s={}
+        x = random.randint(1,7)
+        for i in range(0,x):
+            s[i] = ""
+            y = random.randint(1, 12)
+            for j in range(0,y):
+                if isinstance(self.l[random.randint(0,len(self.l)-1)],str) :
+                    s[i] += self.l[random.randint(0,len(self.l)-1)]
+        for i in range(0, x):
+            print s[i]
 
 
 
+listx = [1,4,5,7,2,7,3,8]
+listx = list(set(listx))
+a = {}
+a[1] = "ty"
+print a[1]
 
 a = listy()
 lista = a.utworz_liste()
+#print lista
 a.losowe_wartosci()
-print len(lista)
-for x in range(0,12):
-    print lista[x]
+print lista
+#for x in range(0,len(lista)):
+#    print lista[x]
+#a.usun_duplikaty()
+#a.duplikaty_na_X()
+#print lista
+#
+a.zastap_wielokrotnosci_235_AEL()
+a.zastap_pierwsze_Z()
+print lista
 
-b = a.wysukaj_wielokrotnosci(2,3,5,7)
-print a.duplikaty_do_nowej(b)
+a.losowe_napisy()
+'''b = a.wysukaj_wielokrotnosci(2,3,5,7)
+print b
+print a.duplikaty_do_nowej(b)'''
+
+'''srednia = a.srednia_i_do_potegi_3()
+print lista
+print srednia'''
+
